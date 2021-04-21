bb070e367337cbe5a621d40d26db23f705425131
Micaella Barcellos Pereira <micaellab30@gmail.com>
2021-04-21 19:51:26 -0300

    aula 3

--- podcastr-next/src/components/Header/index.tsx
+++ podcastr-next/src/components/Header/index.tsx
@@ -1,6 +1,7 @@
 import styles from './styles.module.scss'
 import format from 'date-fns/format'
 import ptBR from 'date-fns/locale/pt-BR'
+import Link from 'next/link'
 
 export function Header() {
   const currentDate = format(new Date(), 'EEEEEE, d MMMM', {
@@ -9,7 +10,11 @@ export function Header() {
 
   return (
     <header className={styles.headerContainer}>
-      <img src="/logo.svg" alt="Podcastr" />
+      <Link href="/">
+        <a>
+          <img src="/logo.svg" alt="Podcastr" />
+        </a>
+      </Link>
 
       <p>O melhor para você ouvir, sempre</p>
 
